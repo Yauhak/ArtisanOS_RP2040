03525d45fcb11b5f93471b13f33908f6ab6d5daf
Yauhak <yauhak_2024@qq.com>
2026-04-05 13:54:07 +0800

    Update Compiler.h

--- Compiler/Source/Compiler.h
+++ Compiler/Source/Compiler.h
@@ -8,7 +8,10 @@
 
 typedef enum {
 	MOV, // 将参数2（立即数或地址，BYTE,INT,FLOAT）存入参数1表示的地址内存
-	IVKARRAY,//访问数组元素
+	EXT_BYTE,//将一个字节类型（变量）扩展为四字节int类型
+	SETARRAY,//设置数组元素
+	READARRAY,//读取数组某个下标的元素
+	INITARRAY,//初始化数组（为数组元素批量赋值）
 	PUSH,//将CalcResu的值保存于参数表示的地址中
 	PUSHP,//为子程序传入参数
 	ADD, // 参数1 + 参数2 → CalcResu
@@ -28,9 +31,12 @@ typedef enum {
 	RET,//子程序返回
 	BIT_AOX,// 按位与、或
 	BIT_MOV,// 按位移
-	TIMER,// 获取系统时钟
+	ARS_TIMER,// 获取系统时钟
 	GPIO_WRITE,// 写GPIO
 	GPIO_READ,// 读GPIO
+	VAL,// 设置临时计算寄存器
+	TO_INT,     // 将内存中的浮点数转换为整数存入 CalcResu
+	TO_FLOAT,    // 将内存中的整数转换为浮点数存入 CalcResu
 	HLT, // 程序结束标记
 } Opcode;
 
